88089d105892933a4e570700837f8239eee38e24
miharu06 <yoshidomi.haruka101@mail.kyutech.jp>
2024-10-31 09:48:43 +0900

    Update search.php

--- search.php
+++ search.php
@@ -7,6 +7,8 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <?php include("dbConfig.php") ?>
+        <?php include("getDatas.php") ?>
         <img src="images/LQ.png" alt="Labo_Quest" width="181" height="33">
         <center>
         <?php
@@ -14,6 +16,11 @@
         ?> 
             <h4 style="text-align: right">更新日：<?php echo $_SESSION['更新日']; ?><br></h4>
             <h1>検索結果</h1>
+            <?php
+                foreach($data as $labo){
+                    echo $labo['labo_name'];
+                };
+            ?>
             
         </center>
     </body>
