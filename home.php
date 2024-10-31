9a962c06fbc2b160803d7999c61ac61d91ca6dc2
miharu06 <yoshidomi.haruka101@mail.kyutech.jp>
2024-10-31 13:30:04 +0900

    Update home.php

--- home.php
+++ home.php
@@ -5,26 +5,22 @@
         <title>Labo Quest</title>
         <meta name="description" content="研究室検索">
         <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <?php include("dbConfig.php") ?>
-        <?php include("getDatas.php") ?>
-        <img src="images/LQ.png" alt="Labo_Quest" width="181" height="33">
-        <center>
+        <a href="home.php">
+            <img src="images/LQ.png" alt="Labo_Quest" width="181" height="33">
+        </a>
         <?php
             session_start();
             //変更時変える
-            $_SESSION['更新日'] = "2024/10/30";
+            $_SESSION['更新日'] = "2024/10/31";
         ?>
+        
+    </head>
+    <body>
+        <center>
 
             <h4 style="text-align: right">更新日：<?php echo $_SESSION['更新日']; ?><br></h4>
             <h1>研究室検索</h1>
-            <!--検索窓-->
-            <form action="search.php" method="POST">
-                <input type="search" name="searchbox" placeholder="キーワードで検索">
-                <input type="image" src="images/q.png" width="20" height="20" alt="検索" value="検索する">
-            </form>
-            <br>
+            <a href="search.php"><h4>検索画面はコチラ▷</h4></a>
             <h2>更新履歴</h2>
             <h4>2024/??/??：九工大工学3類の研究室が検索可能</h4>
         </center>
